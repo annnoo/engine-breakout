85f777fc24951ad8e09da7bd21307be01e65f610
Tebros <info.tebros@web.de>
2018-11-21 00:57:52 +0100

    Add AssetManager with some test files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './css/style.css';
+import AssetManager, {AM_FAILED} from './js/AssetManager/AssetManager';
+
+let am = new AssetManager();
+
+let id_img1 = am.addImage('img_1.png');
+let id_img2 = am.addImage('img_2.png');
+let id_aud1 = am.addAudio('audio_1.wav');
+let id_aud2 = am.addAudio('audio_2.mp3');
+
+am.downloadAll(() => {
+
+    alert('Callback called!');
+    document.body.appendChild(am.getAsset(id_img1).asset);
+
+    document.body.appendChild(am.getAsset(id_aud1).asset);
+
+    let a2 = am.getAsset(id_aud2);
+    document.body.appendChild(a2.asset);
+    a2.asset.play();
+
+    let p = document.createElement('p');
+    p.innerText = "Music by Benjamin TISSOT: https://www.bensound.com/royalty-free-music"
+    document.body.appendChild(p);
+
+    window.console.dir(a2);
+    window.console.log(a2.state);
+    window.console.log(AM_FAILED);
+});
---
+++ src/js/AssetManager/AssetManager.js
@@ -0,0 +1,137 @@
+const AM_DOWNLOADING = 0x01;
+const AM_WAITING = 0x02;
+const AM_FAILED = 0x03;
+const AM_SUCCESS = 0x04;
+
+/**
+ * @author Yannik Ries
+ */
+class AssetManager {
+
+    constructor() {
+        this.idCount = 0;
+        this.cache = [];
+        this.cachePointer = 0;
+        this.reset();
+    }
+
+    /**
+     * Resets the success and fail count.
+     * Does not clear the idCount and cache!
+     */
+    reset() {
+        this.successCount = 0;
+        this.failCount = 0;
+    }
+
+    /**
+     * Adds an asset to the download queue.
+     * You need to trigger the download by yourself.
+     *
+     * @param path Where to find the asset
+     * @param The type of the asset (image, audio)
+     * @returns {number} An unique id of the asset
+     */
+    addAsset(path, assetType) {
+        let id = this.idCount++;
+        this.cache[id] = {
+            'path': path,
+            'assetType': assetType,
+            'state': AM_WAITING
+        };
+        return id;
+    }
+
+    /**
+     * Adds an image asset to the download queue.
+     * You need to trigger the download by yourself.
+     *
+     * @param path Where to find the image asset
+     * @returns {number} An unique id of the image asset
+     */
+    addImage(path) {
+        return this.addAsset(path, 'image');
+    }
+
+    /**
+     * Adds an audio asset to the download queue.
+     * You need to trigger the download by yourself.
+     *
+     * @param path Where to find the audio asset
+     * @returns {number} An unique id of the audio asset
+     */
+    addAudio(path) {
+        return this.addAsset(path, 'audio');
+    }
+
+    /**
+     * Downloads the latest assets of the download queue.
+     *
+     * @param callback Call after all downloads are finished.
+     * Call with a boolean parameter that represents the whole success
+     */
+    downloadAll(callback) {
+
+        for (let i = this.cachePointer; i < this.cache.length - this.cachePointer; i++) {
+
+            let cacheEntry = this.cache[i];
+            let asset = this.createObjectFromType(cacheEntry.assetType);
+
+            asset.addEventListener(asset.loadEventName, () => {
+                this.successCount++;
+                window.console.log('load');
+                if(this.successCount+this.failCount==this.cache.length-this.cachePointer){
+                    cacheEntry.stack = AM_SUCCESS;
+                    callback(this.failCount==0);
+                    this.reset();
+                }
+            });
+            asset.addEventListener(asset.errorEventName, () => {
+                this.failCount++;
+                window.console.error('Could not load asset '+asset.src);
+
+                if(this.successCount+this.failCount==this.cache.length-this.cachePointer){
+                    cacheEntry.stack = AM_FAILED;
+                    callback(this.failCount==0);
+                    this.reset();
+                }
+            });
+            this.cache[i].state = AM_DOWNLOADING;
+            this.cache[i].asset = asset;
+            asset.src = this.cache[i].path;
+        }
+    }
+
+    createObjectFromType(type){
+        window.console.log(type);
+        let asset;
+        switch (type) {
+        case 'image':
+            asset = new Image();
+            asset.loadEventName = 'load';
+            asset.errorEventName = 'error';
+            break;
+        case 'audio':
+            asset = new Audio();
+            asset.loadEventName = 'canplay';
+            asset.errorEventName = 'error';
+            break;
+        }
+        return asset;
+    }
+
+    /**
+     * Returns the cache entry of the given id.
+     *
+     * @param id
+     * @returns {*} Cache entry with downloaded asset
+     */
+    getAsset(id){
+        return this.cache[id];
+    }
+
+}
+
+export default AssetManager;
+
+export {AM_FAILED, AM_SUCCESS, AM_DOWNLOADING, AM_WAITING};
