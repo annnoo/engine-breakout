bb8c0cff0423654ee1e7bdaf31b5b40e7d8e4789
Tebros <info.tebros@web.de>
2018-11-20 16:51:03 +0100

    Fix eslint error with import keyword; Generate source-map for webpack bundle

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "parserOptions": {
-        "ecmaVersion": 2015
+        "ecmaVersion": 2015,
+        "sourceType": "module"
     },
     "rules": {
         "indent": [
