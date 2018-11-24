be2917145a8d4824fb8740dbbc2540e8a402d90e
Tebros <info.tebros@web.de>
2018-11-24 12:17:43 +0100

    Physic added; Rendering abstracted

---
+++ src/js/Renderer/Renderable.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * Base class for renderable stuff
+ *
+ * @author Christian Danscheid
+ */
+class Renderable {
+
+    /**
+     *
+     * @param {CanvasRenderingContext2D} context
+     */
+    draw(context){
+    }
+}
+
+export default Renderable;
