b96886cd59df063d8789d9e4da784556eeb74f03
Christian Danscheid <christian.danscheid@outlook.de>
2018-12-04 13:32:52 +0100

    add pagination to level selection

--- src/js/Breakout/Scenes/LevelSelectScene/LevelSelectScene.js
+++ src/js/Breakout/Scenes/LevelSelectScene/LevelSelectScene.js
@@ -20,6 +20,7 @@ class MainMenuScene extends DOMScene {
     /**
      * Args:
      * imgURI = URI of img to show
+     * page = page number to show
      * selectedLevelID = ID of currently selected level
      *
      * @override
@@ -30,6 +31,37 @@ class MainMenuScene extends DOMScene {
 
         // Parse scene args
         this.selectedLevel = args.selectedLevelID;
+        this.displayPage = args.page ? args.page : 1;
+
+        this.args = args;
+
+        this.levels = [
+            {id: 1, name: 'Level 1'},
+            {id: 2, name: 'Level 2'},
+            {id: 3, name: 'Level 3'},
+            {id: 4, name: 'Level 4'},
+            {id: 5, name: 'Level 5'},
+            {id: 6, name: 'Level 6'},
+            {id: 7, name: 'Level 7'},
+            {id: 8, name: 'Level 8'},
+            {id: 9, name: 'Level 9'},
+            {id: 10, name: 'Level 10'},
+        ];
+
+        this.pageSize = 4;
+        this.firstPage = 1;
+        this.lastPage = Math.ceil(this.levels.length / this.pageSize);
+
+        const levels = this.levels.slice((this.displayPage - 1) * this.pageSize, (this.displayPage - 1) * this.pageSize + this.pageSize);
+
+        levels.forEach(level => {
+            const levelButton = document.createElement('button');
+            levelButton.classList = 'level-changer button button-orange';
+            levelButton.dataset.levelId = level.id;
+            levelButton.innerHTML = level.name;
+
+            node.querySelector('.items').appendChild(levelButton);
+        });
 
         // Back button
         node.querySelector('#back-button').addEventListener('click', this._backToMainMenu.bind(this));
@@ -44,6 +76,33 @@ class MainMenuScene extends DOMScene {
                 element.classList.replace('button-orange', 'button-red');
             }
         });
+
+        // Nav buttons
+        node.querySelector('#next-page').addEventListener('click', (() => {
+            this._switchPage(this.displayPage + 1);
+        }).bind(this));
+
+        node.querySelector('#before-page').addEventListener('click', (() => {
+            this._switchPage(this.displayPage - 1);
+        }).bind(this));
+
+        node.querySelector('#first-page').addEventListener('click', (() => {
+            this._switchPage(1);
+        }).bind(this));
+
+        node.querySelector('#last-page').addEventListener('click', (() => {
+            this._switchPage(this.lastPage);
+        }).bind(this));
+
+        if (this.displayPage === this.firstPage) {
+           node.querySelector('#first-page').disabled = true;
+           node.querySelector('#before-page').disabled = true;
+        }
+
+        if (this.displayPage === this.lastPage) {
+            node.querySelector('#last-page').disabled = true;
+            node.querySelector('#next-page').disabled = true;
+         }
 
         // Teaser image
         node.querySelector(TEASER_IMG).src = (args.imgURI) ? args.imgURI : 'http://placehold.it/1920x1080';
@@ -73,7 +132,11 @@ class MainMenuScene extends DOMScene {
     }
 
     _selectLevel(id) {
-        this.app.getSceneManager().activateScene(SceneNames.LEVEL_SELECT, { selectedLevelID: id });
+        this.app.getSceneManager().activateScene(SceneNames.LEVEL_SELECT, { ...this.args, selectedLevelID: id });
+    }
+
+    _switchPage(number) {
+        this.app.getSceneManager().activateScene(SceneNames.LEVEL_SELECT, { ...this.args, page: number });
     }
 
     _backToMainMenu() {
